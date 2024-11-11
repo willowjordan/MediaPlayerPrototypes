bd3296e872bc18b48989fff65266cdc11676ab3d
willowjordan <willowtjordan@gmail.com>
2024-11-11 14:46:02 -0500

    sql interop: added database row user control

--- MediaPlayerPrototype2-SQLInteroperability/MediaPlayerPrototype2-SQLInteroperability/MainWindow.xaml.cs
+++ MediaPlayerPrototype2-SQLInteroperability/MediaPlayerPrototype2-SQLInteroperability/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -21,20 +22,41 @@ namespace MediaPlayerPrototype2_SQLInteroperability
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        int nextID = 0;
-        SQLiteConnection connection;
+        static int nextID = 0;
+        public static SQLiteConnection connection;
 
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
-            connection = new SQLiteConnection("Data Source=C:/Users/wtjor/source/repos/MEDIA_PLAYER_PROTOTYPES/MediaPlayerPrototypes/MediaPlayerPrototype2-SQLInteroperability/MediaPlayerPrototype2-SQLInteroperability/database.sqlite");
-            connection.Open();
+            IEnumerable<string> result = Directory.EnumerateFiles("C:/Users/wtjor/source/repos/MEDIA_PLAYER_PROTOTYPES/MediaPlayerPrototypes/MediaPlayerPrototype2-SQLInteroperability/MediaPlayerPrototype2-SQLInteroperability", "database.sqlite");
+            if (result.ToArray().Length == 0) //database doesn't exist
+                connection = new SQLiteConnection("Data Source=C:/Users/wtjor/source/repos/MEDIA_PLAYER_PROTOTYPES/MediaPlayerPrototypes/MediaPlayerPrototype2-SQLInteroperability/MediaPlayerPrototype2-SQLInteroperability/database.sqlite");
+            else
+                connection = new SQLiteConnection("Data Source=C:/Users/wtjor/source/repos/MEDIA_PLAYER_PROTOTYPES/MediaPlayerPrototypes/MediaPlayerPrototype2-SQLInteroperability/MediaPlayerPrototype2-SQLInteroperability/database.sqlite;New=False");
+            try
+            {
+                connection.Open();
+                string createTable = @"CREATE TABLE items(
+                                    id INTEGER PRIMARY KEY,
+                                    name TEXT NOT NULL
+                                    );";
+                SQLiteCommand command = new SQLiteCommand(createTable, connection);
+                command.ExecuteNonQuery();
+
+            }
+            catch (SQLiteException except)
+            {
+                // Display the exception
+                Console.WriteLine(except.Message);
+            }
+            connection.Close();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-
+        
         private string input_text;
 
         public string InputText
@@ -42,18 +64,33 @@ namespace MediaPlayerPrototype2_SQLInteroperability
             get { return input_text; }
             set {
                 input_text = value;
-                OnPropertyChanged(input_text);
+                OnPropertyChanged();
             }
         }
-
+        
         private void OnPropertyChanged([CallerMemberName] string PropertyName = null)
         {
             PropertyChanged.Invoke(this, new PropertyChangedEventArgs(PropertyName));
         }
-
+        
         private void Add_Button_Click(object sender, RoutedEventArgs e)
         {
-            InputText = "";
+            try
+            {
+                connection.Open();
+                string addInputText = "INSERT INTO items VALUES (@nextID, @InputText)";
+                SQLiteCommand insertInputCmd = new SQLiteCommand(addInputText, connection);
+                insertInputCmd.Parameters.AddWithValue("@nextID", nextID);
+                insertInputCmd.Parameters.AddWithValue("@InputText", InputText);
+                insertInputCmd.ExecuteNonQuery();
+            }
+            catch (SQLiteException except)
+            {
+                // Display the exception
+                Console.WriteLine(except.Message);
+            }
+            txtInput.Clear();
+            connection.Close();
         }
     }
 }
---
+++ MediaPlayerPrototype2-SQLInteroperability/MediaPlayerPrototype2-SQLInteroperability/View/UserControls/DatabaseRow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayerPrototype2_SQLInteroperability.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DatabaseRow.xaml
+    /// </summary>
+    public partial class DatabaseRow : UserControl, INotifyPropertyChanged
+    {
+        public DatabaseRow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        //delete this row from the database and destroy this object
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MainWindow.connection.Open();
+                string delete = "DELETE FROM items WHERE id = @id";
+                SQLiteCommand delCmd = new SQLiteCommand(delete, MainWindow.connection);
+                delCmd.Parameters.AddWithValue("@id", ID);
+                delCmd.ExecuteNonQuery();
+                MainWindow.connection.Close();
+            }
+            catch (SQLiteException except)
+            {
+                // Display the exception
+                Console.WriteLine(except.Message);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set { 
+                id = value;
+                OnPropertyChanged();
+            }
+        }
+        private string itemname;
+        public string ItemName
+        {
+            get { return itemname; }
+            set { 
+                itemname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
